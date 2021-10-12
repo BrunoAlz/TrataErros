02bc7537aff4142bf8e883c542dc6bb796d9dff1
Bruno <bruno6821@gmail.com>
2021-10-12 14:27:08 -0300

    Ajuste nos atributos da classe ContaCorrente

--- contas.py
+++ contas.py
@@ -4,9 +4,12 @@ class ContaCorrente:
 
     def __init__(self, cliente, agencia, numero) -> None:
         self.__saldo = 100
+        self.__agencia = 0
+        self.__numero = 0
+        
         self.cliente = cliente
-        self.__agencia = agencia
-        self.__numero = numero
+        self.__set_agencia = agencia
+        self.__set_numero = numero
 
         ContaCorrente.total_contas_criadas += 1
         ContaCorrente.taxa_operacao = (30 / ContaCorrente.total_contas_criadas)
@@ -15,15 +18,12 @@ class ContaCorrente:
     def agencia(self):
         return self.__agencia
 
-    @agencia.setter
-    def agencia(self, valor):
-
+    def __set_agencia(self, valor):
         if not isinstance(valor, int):
             return
 
         if valor <= 0:
             return
-
         self.__agencia = valor
 
 
@@ -31,42 +31,32 @@ class ContaCorrente:
     def numero(self):
         return self.__numero
 
-    @numero.setter
-    def numero(self, valor):
-
+    def __set_numero(self, valor):
         if not isinstance(valor, int):
             return
 
         if valor <= 0:
             return
-
         self.__numero = valor
-        
-        
-        
+
+
     @property
     def saldo(self):
         return self.__saldo
 
-    @saldo.setter
-    def saldo(self, valor):
-
+    def __set_saldo(self, valor):
         if not isinstance(valor, int):
             return
 
         if valor <= 0:
             return
-
         self.__saldo = valor
-                
-        
-        
-        
+
 
     def transferir(self, valor, favorecido):
         favorecido.depositar(valor)
 
-    def secar(self, valor):
+    def sacar(self, valor):
         self.saldo -= valor
 
     def depositar(self, valor):
